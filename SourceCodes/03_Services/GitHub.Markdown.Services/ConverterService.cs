86435df7c9f6a25546c5e9572eef14682e16c6bc
Justin Yoo <justin.yoo@aliencube.com>
2014-10-07 16:33:50 +1100

    Add service layer project

---
+++ SourceCodes/03_Services/GitHub.Markdown.Services/ConverterService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace Aliencube.GitHub.Markdown.Services
+{
+    public class ConverterService
+    {
+        private readonly IConnection _connection;
+
+        public ConverterService()
+        {
+        }
+
+        public async Task<string> ConvertAsync(string markdown)
+        {
+            var html = String.Empty;
+            if (String.IsNullOrWhiteSpace(markdown))
+            {
+                return html;
+            }
+
+            using (var client = new GitHubClientWrapper(this._connection))
+            {
+                html = await client.Miscellaneous.RenderRawMarkdown(markdown);
+            }
+            return html;
+        }
+    }
+}
---
+++ SourceCodes/03_Services/GitHub.Markdown.Services/GitHubClientWrapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aliencube.GitHub.Markdown.Services.Interfaces;
+using Octokit;
+
+namespace Aliencube.GitHub.Markdown.Services
+{
+    public class GitHubClientWrapper : GitHubClient, IGitHubClientWrapper
+    {
+
+        public GitHubClientWrapper(IConnection connection)
+            : base(connection)
+        {
+        }
+
+        public GitHubClientWrapper(ProductHeaderValue productInformation)
+            : base(productInformation)
+        {
+        }
+
+        public GitHubClientWrapper(ProductHeaderValue productInformation, ICredentialStore credentialStore)
+            : base(productInformation, credentialStore)
+        {
+        }
+
+        public GitHubClientWrapper(ProductHeaderValue productInformation, Uri baseAddress)
+            : base(productInformation, baseAddress)
+        {
+        }
+
+        public GitHubClientWrapper(ProductHeaderValue productInformation, ICredentialStore credentialStore, Uri baseAddress)
+            : base(productInformation, credentialStore, baseAddress)
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
