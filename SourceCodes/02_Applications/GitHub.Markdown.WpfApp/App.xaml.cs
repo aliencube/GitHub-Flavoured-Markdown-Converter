cfdfe013c3c4ab1554d64ab1d51ad9e4e98521f0
Justin Yoo <justin.yoo@aliencube.com>
2014-10-08 13:53:59 +1100

    Add WPF app project

---
+++ SourceCodes/02_Applications/GitHub.Markdown.WpfApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Aliencube.GitHub.Markdown.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ SourceCodes/02_Applications/GitHub.Markdown.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Aliencube.GitHub.Markdown.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Convert_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
