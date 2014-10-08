89f4fac2102eed160921f4735d3ccd5079694122
Justin Yoo <justin.yoo@aliencube.com>
2014-10-08 15:24:13 +1100

    Update MainWindow to convert markdown to HTML

--- SourceCodes/02_Applications/GitHub.Markdown.WpfApp/MainWindow.xaml.cs
+++ SourceCodes/02_Applications/GitHub.Markdown.WpfApp/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using Aliencube.GitHub.Markdown.Configurations;
+using Aliencube.GitHub.Markdown.Configurations.Interfaces;
+using Aliencube.GitHub.Markdown.Services;
+using Aliencube.GitHub.Markdown.Services.Interfaces;
+using System.Configuration;
+using System.Windows;
 
 namespace Aliencube.GitHub.Markdown.WpfApp
 {
@@ -7,15 +12,24 @@ namespace Aliencube.GitHub.Markdown.WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly IGitHubClientSettings _settings;
+        private readonly IGitHubClientHelper _helper;
+        private readonly IConverterService _converter;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            this._settings = ConfigurationManager.GetSection("gitHubClientSettings") as GitHubClientSettings;
+            this._helper = new GitHubClientHelper(this._settings);
+            this._converter = new ConverterService(this._helper);
         }
 
-        private void Convert_Click(object sender, RoutedEventArgs e)
+        private async void Convert_Click(object sender, RoutedEventArgs e)
         {
-
+            var input = this.Input.Text;
+            var output = await this._converter.ConvertAsync(input);
+            this.Output.NavigateToString(output);
         }
     }
 }
