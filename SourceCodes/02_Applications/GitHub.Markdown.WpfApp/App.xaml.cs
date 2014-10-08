77ec454058ab0c63e4752fdb9233678c3d3e249b
Justin Yoo <justin.yoo@aliencube.com>
2014-10-08 16:34:39 +1100

    Remove Autofac related code bits

--- SourceCodes/02_Applications/GitHub.Markdown.WpfApp/App.xaml.cs
+++ SourceCodes/02_Applications/GitHub.Markdown.WpfApp/App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System.Configuration;
-using System.Windows;
-using Aliencube.GitHub.Markdown.Configurations;
-using Aliencube.GitHub.Markdown.Configurations.Interfaces;
-using Aliencube.GitHub.Markdown.Services;
-using Aliencube.GitHub.Markdown.Services.Interfaces;
-using Autofac;
+﻿using System.Windows;
 
 namespace Aliencube.GitHub.Markdown.WpfApp
 {
@@ -13,17 +7,5 @@ namespace Aliencube.GitHub.Markdown.WpfApp
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-
-            var builder = new ContainerBuilder();
-
-            builder.Register(p => ConfigurationManager.GetSection("gitHubClientSettings") as GitHubClientSettings).As<IGitHubClientSettings>().InstancePerLifetimeScope();
-            builder.RegisterType<GitHubClientHelper>().As<IGitHubClientHelper>().InstancePerLifetimeScope();
-            builder.RegisterType<ConverterService>().As<IConverterService>().PropertiesAutowired().InstancePerLifetimeScope();
-
-            builder.Build();
-        }
     }
 }
--- SourceCodes/02_Applications/GitHub.Markdown.WpfApp/MainWindow.xaml.cs
+++ SourceCodes/02_Applications/GitHub.Markdown.WpfApp/MainWindow.xaml.cs
@@ -12,17 +12,23 @@ namespace Aliencube.GitHub.Markdown.WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly IGitHubClientSettings _settings;
+        private readonly IGitHubClientHelper _helper;
+        private readonly IConverterService _converter;
+
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        public IConverterService Converter { get; set; }
+            this._settings = ConfigurationManager.GetSection("gitHubClientSettings") as GitHubClientSettings;
+            this._helper = new GitHubClientHelper(this._settings);
+            this._converter = new ConverterService(this._helper);
+        }
 
         private async void Convert_Click(object sender, RoutedEventArgs e)
         {
             var input = this.Input.Text;
-            var output = await this.Converter.ConvertAsync(input);
+            var output = await this._converter.ConvertAsync(input);
             this.Output.NavigateToString(output);
         }
     }
