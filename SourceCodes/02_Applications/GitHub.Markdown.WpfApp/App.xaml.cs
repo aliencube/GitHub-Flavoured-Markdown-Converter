b5ce40db2066180be663c33c3375b206bf80edc9
Jin-Wook Chung <jwChung@hotmail.com>
2014-10-08 16:36:03 +0900

    Address implementing manual DI.

--- SourceCodes/02_Applications/GitHub.Markdown.WpfApp/App.xaml.cs
+++ SourceCodes/02_Applications/GitHub.Markdown.WpfApp/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using Aliencube.GitHub.Markdown.Configurations;
+using Aliencube.GitHub.Markdown.Services;
+using System.Configuration;
+using System.Windows;
 
 namespace Aliencube.GitHub.Markdown.WpfApp
 {
@@ -7,5 +10,17 @@ namespace Aliencube.GitHub.Markdown.WpfApp
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var settings = (GitHubClientSettings)ConfigurationManager
+                .GetSection("gitHubClientSettings");
+            var helper = new GitHubClientHelper(settings);
+            var converter = new ConverterService(helper);
+
+            Application.Current.MainWindow = new MainWindow(settings, helper, converter);
+            Application.Current.MainWindow.Show();
+        }
     }
 }
